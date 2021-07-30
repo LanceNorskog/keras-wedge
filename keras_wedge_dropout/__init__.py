5759f1a76f1aa0193e07753014ad1aee2bc985d1
Lance Norskog <LanceNorskog@users.noreply.github.com>
2021-07-30 02:02:51 -0700

    Update __init__.py

--- keras_wedge_dropout/__init__.py
+++ keras_wedge_dropout/__init__.py
@@ -1,4 +1,4 @@
-from .wedge_dropout import WedgeDropout1D, WedgeDropout2D, WedgeDropout3D
+from .wedge_dropout import WedgeDropout1D, WedgeDropout2D, WedgeDropout3D, WedgeDropoutLSTM2D
 from .functions import wedge_direct_comparison, wedge_normalize
 
 __version__ = '0.1.0'
