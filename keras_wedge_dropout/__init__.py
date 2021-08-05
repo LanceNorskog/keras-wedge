b3057728d02a1ffe4fa8f27c280cedf62aba91b8
Lance Norskog <LanceNorskog@users.noreply.github.com>
2021-08-04 23:12:48 -0700

    Export more functions

--- keras_wedge_dropout/__init__.py
+++ keras_wedge_dropout/__init__.py
@@ -1,4 +1,4 @@
 from .wedge_dropout import WedgeDropout1D, WedgeDropout2D, WedgeDropout3D, WedgeDropoutLSTM2D
-from .functions import wedge_direct_comparison, wedge_normalize
+from .functions import wedge_direct_comparison, wedge_normalize, wedge_standardize1d, wedge_standardize2d
 
 __version__ = '0.1.0'
